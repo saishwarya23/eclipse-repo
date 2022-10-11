3141d15d2e70979544e492f7c304bf974649b650
Aishu <saishwarya.xworkz@gmail.com>
2022-10-11 18:17:00 +0530

    Constructor

---
+++ first/src/com/xworkz/amount/boot/xworkz.java
@@ -0,0 +1,9 @@
+package com.xworkz.amount.boot;
+
+public class xworkz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This Is First Eclipse Program");	}
+
+}
