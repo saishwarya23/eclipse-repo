2bba399f71ad5f3d4e0624b5108ad2d84579fe52
Aishu <saishwarya.xworkz@gmail.com>
2022-10-12 14:51:41 +0530

    oct 12

---
+++ ThirdTask/src/com/xworkz/copy/things/Cigaratte.java
@@ -0,0 +1,23 @@
+package com.xworkz.copy.things;
+
+public class Cigaratte {
+
+	public String color="white";
+	public String name="King";
+	public String brand="Gold Flakes";
+	public String founder="Stephen Mitchell";
+	public String flavour;
+	public double price=180;
+	public String smell;
+	public String shopName;
+	public String manufactureDate;
+	public String ownerName;
+	public String type="Hooka";
+	public int quantity=20;
+	public boolean quality;
+	public String expiryDate;
+	public boolean taste;
+	public String country;
+	public String Manufacturer="Public Limited Company";
+	public String companyName="India";
+}
---
+++ ThirdTask/src/com/xworkz/copy/things/Tea.java
@@ -0,0 +1,26 @@
+package com.xworkz.copy.things;
+
+public class Tea {
+	
+	
+	public String color="Reddish Orange";
+	public String name="Ginger Tea";
+	public String flavour="Ginger";
+	public String type;
+	public String brand;
+	public String shopName="Tea Stall";
+	public String ownerName="Narendra modi";
+	public String fatherName;
+	public String motherName;
+	public String cupType="Glass";
+	public String founder;
+	public double price=10;
+	public String quantity;
+	public boolean taste;
+	public String teaPlantName;
+	
+	public Tea()
+	{
+		System.out.println("Running Tea Constructor");
+	}
+}
